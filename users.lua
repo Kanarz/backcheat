f29d766499e998b5b61d27d276a9596d38e593cf
Kanarz <107653172+Kanarz@users.noreply.github.com>
2024-12-18 16:37:42 -0500

    Update users.lua

--- users.lua
+++ users.lua
@@ -1,1 +1,1 @@
-local v0={7266327275,2213383572,4286398036};users.get=function() for v2,v3 in pairs(v0) do print(v3);end end;return v0;
+local v0={7266327275,2213383572,4286398036};v0.get=function() for v2,v3 in pairs(v0) do print(v3);end end;return v0;
